221f116dc8dce716cabfc7e89cff201d1a09e7d8
Dave Longley <dlongley@digitalbazaar.com>
2019-03-03 18:06:15 -0500

    Fix API tests.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+/*!
+ * Copyright (c) 2018-2019 Digital Bazaar, Inc. All rights reserved.
  */
 /* jshint node: true */
 
@@ -22,61 +22,117 @@ accounts[email].meta.sysResourceRole = [{
   generateResource: 'id'
 }];
 
-data.masterKey = {
-  unprotected: {
-    alg: 'PBES2-HS512+A256KW',
-    p2c: 4096,
-    p2s: 'd7l6Ub5T0eZlpWjhSGI3Q19DtcogEkHg1hN8JzORj4U'
+data.config = {
+  "id": "e77fff16-3e0e-11e9-ab58-10bf48838a41",
+  "sequence": 0,
+  "kek": {
+    "id": "f3ce1a43-162f-43b2-80b4-b51abbeea46e",
+    "algorithm": "A256KW"
   },
-  encrypted_key:
-    'HrLOox-iCFlwCsQIWAWJ7UCuzjt2jdzOv92rEFNYymNX0XiIE_k8U-' +
-    'z_Y3kCc_xqQ_wob904Q3XJxwzsO6xla7plr54MVh0N'
+  "hmac": {
+    "id": "13a91b49-3bd6-4e7a-aacd-4a1e0468bb2e",
+    "algorithm": "HS256"
+  },
+  "primary": true
 };
 
-data.encryptedDocument = {
-  id: 'lcm5RDZGuDmxlFZSc0k468LcfiY0viSxm7UIBFPKKVk',
-  attributes: [],
-  jwe: {
-    unprotected: {
-      alg: 'A256KW',
-      enc: 'A256GCM'
-    },
-    encrypted_key: 'BZHh3ExmG56fJtGbb3L_tfJe9WNQHaDK1XmmO807pjoFuuDktdqfUQ',
-    iv: 'PcpoGFRyHYBuPIu1',
-    ciphertext: 'RR1VJiV16uxzjbyaprspvAuuso2J2AnB8GgbQvu07D56IA',
-    tag: 'SHj-mrvbuLWLLTWW8EQ_8w'
+data.doc1 = {
+  "id": "foo",
+  "sequence": 0,
+  "indexed": [
+    {
+      "hmac": {
+        "id": "13a91b49-3bd6-4e7a-aacd-4a1e0468bb2e",
+        "algorithm": "HS256"
+      },
+      "sequence": 0,
+      "attributes": []
+    }
+  ],
+  "jwe": {
+    "protected": "eyJlbmMiOiJDMjBQIn0",
+    "recipients": [
+      {
+        "header": {
+          "alg": "A256KW",
+          "kid": "f3ce1a43-162f-43b2-80b4-b51abbeea46e"
+        },
+        "encrypted_key":
+          "HM00migkUSdZjvqmq4b7ixiXnfeLieA7QX2ew6OF4oPUA3HovaMnOw"
+      }
+    ],
+    "iv": "S-bNe9DayHcXWhBH",
+    "ciphertext": "bcZnPyreRmcLCngVbMHJTNeIIxkSJno",
+    "tag": "R2xDL9AJo7IhZ7y_sebgJw"
   }
 };
 
-data.encryptedDocument2 = {
-  id: 'lcm5RDZGuDmxlFZSc0k468LcfiY0viSxm7UIBFPKKVj',
-  attributes: [],
-  jwe: {
-    unprotected: {
-      alg: 'A256KW',
-      enc: 'A256GCM'
-    },
-    encrypted_key: 'BZHh3ExmG56fJtGbb3L_tfJe9WNQHaDK1XmmO807pjoFuuDktdqfUQ',
-    iv: 'PcpoGFRyHYBuPIu1',
-    ciphertext: 'RR1VJiV16uxzjbyaprspvAuuso2J2AnB8GgbQvu07D56IA',
-    tag: 'SHj-mrvbuLWLLTWW8EQ_8w'
+data.doc2 = {
+  "id": "foo2",
+  "sequence": 0,
+  "indexed": [
+    {
+      "hmac": {
+        "id": "13a91b49-3bd6-4e7a-aacd-4a1e0468bb2e",
+        "algorithm": "HS256"
+      },
+      "sequence": 0,
+      "attributes": []
+    }
+  ],
+  "jwe": {
+    "protected": "eyJlbmMiOiJDMjBQIn0",
+    "recipients": [
+      {
+        "header": {
+          "alg": "A256KW",
+          "kid": "f3ce1a43-162f-43b2-80b4-b51abbeea46e"
+        },
+        "encrypted_key":
+          "HM00migkUSdZjvqmq4b7ixiXnfeLieA7QX2ew6OF4oPUA3HovaMnOw"
+      }
+    ],
+    "iv": "S-bNe9DayHcXWhBH",
+    "ciphertext": "bcZnPyreRmcLCngVbMHJTNeIIxkSJno",
+    "tag": "R2xDL9AJo7IhZ7y_sebgJw"
   }
 };
 
-data.encryptedDocumentWithAttribute = {
-  id: 'SAvbGbH82THrB4ZbJkPpg0hgAyqQ18OwVJ3PU7yy_pA',
-  attributes: [{
-    name: 'R-s3X4fp3zyXa_xUqoefYLl22xTpnEUa-vGps0SRZqc',
-    value: 'telhiiyV0Cnf5tVeZQ7gQS4h6Lr60hBg1yx1DTT5ir4'
-  }],
-  jwe: {
-    unprotected: {
-      alg: 'A256KW',
-      enc: 'A256GCM'
-    },
-    encrypted_key: '30Pri80iyLFvDNvhVbOAgxvYVRyRygc5VAiIFEcSiZ5EaavpuQzU6g',
-    iv: '5IoIPcoZfWBOnguz',
-    ciphertext: 'KHuPT57SzalurZ2X5Mq0n5G21NuygCsqQtIgCC8hTqdKRkKjjgLE6O9KtpJW',
-    tag: 'BxzhA3kMExvNLI14rStPWQ'
+data.docWithAttributes = {
+  "id": "hasAttributes1",
+  "sequence": 0,
+  "indexed": [
+    {
+      "hmac": {
+        "id": "13a91b49-3bd6-4e7a-aacd-4a1e0468bb2e",
+        "algorithm": "HS256"
+      },
+      "sequence": 0,
+      "attributes": [
+        {
+          "name": "CUQaxPtSLtd8L3WBAIkJ4DiVJeqoF6bdnhR7lSaPloY",
+          "value": "QV58Va4904K-18_L5g_vfARXRWEB00knFSGPpukUBro"
+        }
+      ]
+    }
+  ],
+  "jwe": {
+    "protected": "eyJlbmMiOiJDMjBQIn0",
+    "recipients": [
+      {
+        "header": {
+          "alg": "A256KW",
+          "kid": "f3ce1a43-162f-43b2-80b4-b51abbeea46e"
+        },
+        "encrypted_key":
+          "OR1vdCNvf_B68mfUxFQVT-vyXVrBembuiM40mAAjDC1-Qu5iArDbug"
+      }
+    ],
+    "iv": "i8Nins2vTI3PlrYW",
+    "ciphertext": "Cb-963UCXblINT8F6MDHzMJN9EAhK3I",
+    "tag": "pfZO0JulJcrc3trOZy8rjA"
+  },
+  "content": {
+    "indexedKey": "value1"
   }
 };
